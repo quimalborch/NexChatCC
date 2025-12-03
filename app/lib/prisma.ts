b063d03ca3887c72d609c0504e716a98558a410b
Quim Alborch <quimalborch@gmail.com>
2025-12-03 18:39:40 +0100

    Corregir la importación de PrismaClient y asegurar que la URL de la base de datos esté definida

--- app/lib/prisma.ts
+++ app/lib/prisma.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '../../prisma/generated/prisma-client'
+import { PrismaClient } from '../../generated/prisma/client'
 import { withAccelerate } from '@prisma/extension-accelerate'
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
@@ -6,7 +6,7 @@ const globalForPrisma = global as unknown as { prisma: PrismaClient };
 export const prisma =
   globalForPrisma.prisma ||
   new PrismaClient({
-    accelerateUrl: process.env.PRISMA_DATABASE_URL
+    accelerateUrl: process.env.PRISMA_DATABASE_URL!
   }).$extends(withAccelerate());
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
